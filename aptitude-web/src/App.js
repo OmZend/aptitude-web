a4bae0c801c4edd2813ae3b7744f2eff7afbebde
Roshan02-arch <rp103339@gmail.com>
2025-03-09 22:25:03 +0530

    Resolved merge conflicts

--- aptitude-web/src/App.js
+++ aptitude-web/src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Quantitative from "./pages/Quantitative";
+import Logical from "./pages/Logical";
+import Verbal from "./pages/Verbal";
+import DataInterpretation from "./pages/DataInterpretation";
+import GeneralKnowledge from "./pages/GeneralKnowledge";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Aptitude arya roshan</h1>
-      </header>
-    </div>
+    <Router>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ marginLeft: "260px", padding: "20px", flex: 1 }}>
+          <Routes>
+            <Route path="/quantitative" element={<Quantitative />} />
+            <Route path="/logical" element={<Logical />} />
+            <Route path="/verbal" element={<Verbal />} />
+            <Route path="/data-interpretation" element={<DataInterpretation />} />
+            <Route path="/general-knowledge" element={<GeneralKnowledge />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
